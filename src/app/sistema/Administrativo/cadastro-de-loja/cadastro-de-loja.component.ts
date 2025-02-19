271a86406dcd9da7302d17ffc2721b0416660fd6
vctrwesley <vctrwesley@gmail.com>
2025-02-19 00:23:06 -0300

    base do cadastro de loja

--- src/app/sistema/Administrativo/cadastro-de-loja/cadastro-de-loja.component.ts
+++ src/app/sistema/Administrativo/cadastro-de-loja/cadastro-de-loja.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-de-loja',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroDeLojaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
