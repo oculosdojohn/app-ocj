849d9bb57ef7ca60ce724fe9ca81617295e46874
vctrwesley <vctrwesley@gmail.com>
2025-02-19 17:05:01 -0300

    Criando componente de imagens geral e adicionando no cadastro

--- src/app/sistema/Administrativo/cadastro-de-colaborador/cadastro-de-colaborador.component.ts
+++ src/app/sistema/Administrativo/cadastro-de-colaborador/cadastro-de-colaborador.component.ts
@@ -4,19 +4,21 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-cadastro-de-colaborador',
   templateUrl: './cadastro-de-colaborador.component.html',
-  styleUrls: ['./cadastro-de-colaborador.component.css']
+  styleUrls: ['./cadastro-de-colaborador.component.css'],
 })
 export class CadastroDeColaboradorComponent implements OnInit {
+  selectedImages: { [key: string]: File | null } = {};
 
-  constructor(
-    private location: Location
-  ) { }
+  constructor(private location: Location) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goBack() {
     this.location.back();
   }
 
+  onImageSelected(image: File | null, tipo: string) {
+    this.selectedImages[tipo] = image;
+    console.log(`Imagem de ${tipo} selecionada:`, image);
+  }
 }
