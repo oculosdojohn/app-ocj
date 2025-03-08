53e3385d8bc2b3b2c5d0051f48a710ff04e50d17
vctrwesley <vctrwesley@gmail.com>
2025-03-08 20:00:45 -0300

    montando class aula e adicionando as enum dos modluos de aulas

---
+++ src/app/sistema/Servicos/cursos/enums/modulos-descricao.ts
@@ -0,0 +1,20 @@
+import { Modulos } from './modulos';
+
+export const ModulosDescricao: Record<Modulos, string> = {
+    [Modulos.ONBOARDING]: 'Onboarding',
+    [Modulos.HISTORIA_SUCESSO]: 'História de sucesso - Grupo OCJ',
+    [Modulos.PRINCIPIOS_OTICA]: 'Princípios básicos de Ótica',
+    [Modulos.SCRIPT_VENDAS]: 'Script de vendas',
+    [Modulos.CONSEGUIR_CLIENTES]: 'Como conseguir clientes',
+    [Modulos.CONSULTOR_ALTA_PERFORMANCE]: 'Consultor Ótico de alta performance',
+    [Modulos.LIMPEZA_MANUTENCAO]: 'Limpeza e manutenção dos produtos',
+    [Modulos.MANUTENCAO_OCULOS]: 'Manutenção de óculos',
+    [Modulos.GARANTIA_PRODUTOS]: 'Garantia de produtos',
+    [Modulos.EMBALAGEM_PADRAO]: 'Embalagem padrão dos produtos',
+    [Modulos.ENTREGA_OCULOS_GRAU]: 'Entrega de óculos de grau',
+    [Modulos.PADROES_ATENDIMENTO]: 'Padrões de atendimento',
+    [Modulos.SSOTICA_SISTEMA_VENDAS]: 'SSotica sistema de vendas',
+    [Modulos.SSOTICA_CAIXA]: 'SSotica (caixa)',
+    [Modulos.EU_SOU_VENDEDOR]: 'Eu sou vendedor',
+    [Modulos.INTELIGENCIA_EMOCIONAL]: 'Inteligência emocional',
+};
