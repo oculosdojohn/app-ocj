4fb89d259d608b06230e2c8e98bb0cc942e3baf9
vctrwesley <vctrwesley@gmail.com>
2025-03-09 23:44:54 -0300

    tabela e tela de registros

--- src/app/sistema/RH/registros/registros.component.ts
+++ src/app/sistema/RH/registros/registros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Registro } from './registro';
 
 @Component({
   selector: 'app-registros',
@@ -9,9 +10,27 @@ import { Router } from '@angular/router';
 export class RegistrosComponent implements OnInit {
   termoBusca: string = '';
 
+  registros: Registro[] = [
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' },
+    { data: '22/01/2025', loja: 'Óculos do John LTDA', colaborador: 'Everardo Costta', tipo: 'Advertência', classificacao: 'Negativo', autor: 'Vilma' }
+  ];
+  
+  itensPorPagina = 6;
+  paginaAtual = 1;
+  totalPaginas = Math.ceil(this.registros.length / this.itensPorPagina);
+  registrosPaginados: Registro[] = [];
+
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.atualizarPaginacao();
+  }
 
   cadastrarRegistro(): void {
     this.router.navigate(['/usuario/cadastro-de-registro']);
@@ -19,5 +38,20 @@ export class RegistrosComponent implements OnInit {
 
   onSearch(searchTerm: string) {
     console.log('Search term:', searchTerm);
+  }
+
+  atualizarPaginacao(): void {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    this.registrosPaginados = this.registros.slice(inicio, fim);
+  }
+
+  get totalItens() {
+    return this.registros.length;
+  }
+
+  onPaginaMudou(novaPagina: number) {
+    this.paginaAtual = novaPagina;
+    this.atualizarPaginacao();
   }
 }
