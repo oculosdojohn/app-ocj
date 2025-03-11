acab705ad7f7c17f7de2de8d77f2e643923c6482
vctrwesley <vctrwesley@gmail.com>
2025-03-11 14:37:34 -0300

    tabela de procedimentos medicos

--- src/app/sistema/RH/medicina/medicina.component.ts
+++ src/app/sistema/RH/medicina/medicina.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Medicina } from './medicina';
 
 @Component({
   selector: 'app-medicina',
@@ -9,9 +10,27 @@ import { Router } from '@angular/router';
 export class MedicinaComponent implements OnInit {
   termoBusca: string = '';
 
+  medicinas: Medicina[] = [
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'},
+      { colaborador: 'Everardo Costta', tipo: 'Atestado médico', CID: 'R11 - Náuseas e Vômitos', data: '22/01/2025', numeroDias: '2'}
+  ];
+    
+  itensPorPagina = 6;
+  paginaAtual = 1;
+  totalPaginas = Math.ceil(this.medicinas.length / this.itensPorPagina);
+  medicinasPaginados: Medicina[] = [];
+
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.atualizarPaginacao();
+  }
 
   cadastrarExame(): void {
     this.router.navigate(['/usuario/cadastro-de-procedimentos-medicos']);
@@ -19,5 +38,20 @@ export class MedicinaComponent implements OnInit {
 
   onSearch(searchTerm: string) {
     console.log('Search term:', searchTerm);
+  }
+
+  atualizarPaginacao(): void {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    this.medicinasPaginados = this.medicinas.slice(inicio, fim);
+  }
+
+  get totalItens() {
+    return this.medicinas.length;
+  }
+
+  onPaginaMudou(novaPagina: number) {
+    this.paginaAtual = novaPagina;
+    this.atualizarPaginacao();
   }
 }
