97aa246b6f08f0c0c1f9079ed00dbe171fd84fe3
vctrwesley <vctrwesley@gmail.com>
2025-04-08 21:27:33 -0300

    Adicionando importações e configurando a URL da API no serviço de departamento

--- src/app/services/administrativo/departamento.service.ts
+++ src/app/services/administrativo/departamento.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Departamento } from 'src/app/sistema/Administrativo/departamentos/departamento';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DepartamentoService {
+  apiURL: string = environment.apiURLBase + '/api/departamento';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
