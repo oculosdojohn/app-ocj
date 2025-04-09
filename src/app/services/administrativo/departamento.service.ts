d019fde94949271e720f4cde91b077b52ac8be7e
vctrwesley <vctrwesley@gmail.com>
2025-04-08 22:11:36 -0300

    Adicionando métodos para buscar, atualizar e deletar departamentos com tratamento de erros.

--- src/app/services/administrativo/departamento.service.ts
+++ src/app/services/administrativo/departamento.service.ts
@@ -47,4 +47,57 @@ export class DepartamentoService {
       })
     );
   }
+
+  getDepartamentoById(id: number): Observable<Departamento> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.get<Departamento>(url).pipe(
+      map((response) => response),
+      catchError((error) => {
+        let errorMessage = 'Erro ao buscar a departamento.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  atualizarDepartamento(id: string, departamento: Departamento): Observable<Departamento> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.put<Departamento>(url, departamento).pipe(
+      map((response) => response),
+      catchError((error) => {
+        let errorMessage = 'Erro ao atualizar a departamento.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  deleteDepartamentoById(id: string): Observable<void> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.delete<void>(url).pipe(
+      catchError((error) => {
+        let errorMessage = 'Erro ao deletar a departamento.';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro: ${error.error.message}`;
+        } else if (error.status) {
+          errorMessage = `Erro no servidor: ${error.status} - ${error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
 }
