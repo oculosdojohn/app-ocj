1281bcf5748daf3d61f1167026d2e9a7dfb1b603
vctrwesley <vctrwesley@gmail.com>
2025-04-13 16:13:22 -0300

    Implementando formulário de cadastro de colaborador com validação e mensagens de erro

--- src/app/sistema/Administrativo/cadastro-de-colaborador/cadastro-de-colaborador.component.ts
+++ src/app/sistema/Administrativo/cadastro-de-colaborador/cadastro-de-colaborador.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { LojaService } from '../../../services/administrativo/loja.service';
+import { DepartamentoService } from '../../../services/administrativo/departamento.service';
+import { ColaboradorService } from 'src/app/services/administrativo/colaborador.service';
 
 @Component({
   selector: 'app-cadastro-de-colaborador',
@@ -8,13 +12,32 @@ import { LojaService } from '../../../services/administrativo/loja.service';
   styleUrls: ['./cadastro-de-colaborador.component.css'],
 })
 export class CadastroDeColaboradorComponent implements OnInit {
+  colaboradorForm: FormGroup;
+  isLoading = false;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+  isEditMode = false;
   selectedImages: { [key: string]: File | null } = {};
   status: string = 'ativo';
   selectedArquivos: File[] = [];
   lojas: { value: string; description: string }[] = [];
   selectedLoja: string = '';
 
-  constructor(private location: Location, private lojaService: LojaService) {}
+  constructor(
+    private location: Location, 
+    private lojaService: LojaService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private departamentoService: DepartamentoService,
+    private colaboradorService: ColaboradorService
+  ) {
+    this.colaboradorForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      dataNascimento: ['', Validators.required],
+
+    });
+  }
 
   ngOnInit(): void {
     this.carregarLojas();
@@ -51,5 +74,16 @@ export class CadastroDeColaboradorComponent implements OnInit {
   atualizarLojas(): void {
     console.log('Atualizando lista de lojas...');
     this.carregarLojas();
+  }
+
+  onSubmit(): void {}
+
+  isRequired(controlName: string): boolean {
+    const control = this.colaboradorForm.get(controlName);
+    if (control && control.validator) {
+      const validator = control.validator({} as AbstractControl);
+      return !!(validator && validator['required']);
+    }
+    return false;
   }
 }
