7518bfea5e40cae0875335b19e5a1c54b0ca4801
vctrwesley <vctrwesley@gmail.com>
2025-05-05 18:28:25 -0300

    Implementando a página de detalhes do colaborador com exibição de informações e navegação

--- src/app/sistema/Administrativo/vizualizar-adm/detalhes-colaborador/detalhes-colaborador.component.ts
+++ src/app/sistema/Administrativo/vizualizar-adm/detalhes-colaborador/detalhes-colaborador.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Colaborador } from '../../funcionarios/colaborador';
+import { ColaboradorService } from 'src/app/services/administrativo/colaborador.service';
+import { Departamento } from '../../../../sistema/Administrativo/departamentos/departamento';
+import { CargoDescricoes } from '../../../../sistema/Administrativo/funcionarios/enums/cargo-descricoes';
 
 @Component({
   selector: 'app-detalhes-colaborador',
   templateUrl: './detalhes-colaborador.component.html',
-  styleUrls: ['./detalhes-colaborador.component.css']
+  styleUrls: ['./detalhes-colaborador.component.css'],
 })
 export class DetalhesColaboradorComponent implements OnInit {
+  colaborador!: Colaborador;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private colaboradorService: ColaboradorService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.carregarUsuario();
   }
 
+  goBack() {
+    this.location.back();
+  }
+
+  getDescricaoCargo(cargo: string): string {
+    return (
+      CargoDescricoes[cargo as keyof typeof CargoDescricoes] ||
+      'Cargo desconhecido'
+    );
+  }
+
+  carregarUsuario(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.colaboradorService.getColaboradorById(id).subscribe(
+        (response) => {
+          this.colaborador = response;
+          console.log('Dados de usuario carregados:', this.colaborador);
+        },
+        (error) => {
+          console.error('Erro ao carregar os dados do usuario:', error);
+        }
+      );
+    }
+  }
+
+  getInitial(name: string): string {
+    return name ? name.charAt(0).toUpperCase() : '?';
+  }
+
+  getRandomColor(seed: string): string {
+    const colors = [
+      '#FFB3BA', // Rosa pastel
+      '#FFDFBA', // Laranja pastel
+      '#BAFFC9', // Verde pastel
+      '#BAE1FF', // Azul pastel
+      '#D5BAFF', // Roxo pastel
+    ];
+    const index = seed ? seed.charCodeAt(0) % colors.length : 0;
+    return colors[index];
+  }
 }
