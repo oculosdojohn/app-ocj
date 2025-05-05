7ff55cc9c4011bc404a48795640a259db630ac51
vctrwesley <vctrwesley@gmail.com>
2025-05-05 18:31:48 -0300

    Adicionando o componente de navegação de setor com template e estilos

---
+++ src/app/shared/sector-navigation/sector-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sector-navigation',
+  templateUrl: './sector-navigation.component.html',
+  styleUrls: ['./sector-navigation.component.css']
+})
+export class SectorNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
