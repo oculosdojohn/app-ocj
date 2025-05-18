af0dcb7475272bab2c0763beb3ac369612ce08bc
DeniseRisia <risiaresponde@gmail.com>
2025-05-18 14:56:32 -0300

    mr 01-fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
+import { AuthGuard } from './services/configs/auth.guard';
+import { LayoutComponent } from './layout/layout.component';
+import { PainelAdminComponent } from './sistema/Dashboard-Usuarios/painel-admin/painel-admin.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'esqueci-minha-senha', component: EsqueciSenhaComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' }
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],         
+    children: [
+      { path: 'dashboard-admin', component: PainelAdminComponent, canActivate: [AuthGuard], data: { roles: ['ROLE_ADMIN', 'ROLE_RH'] } }
+    ]
+  },
 ];
 
 @NgModule({
