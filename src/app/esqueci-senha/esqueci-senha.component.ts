57674f0a4c3b901a0e52118141504620daff312d
vctrwesley <vctrwesley@gmail.com>
2025-05-23 01:18:04 -0300

    adicionando a funcionalidade de recuperação de senha com layout e estilos aprimorados

--- src/app/esqueci-senha/esqueci-senha.component.ts
+++ src/app/esqueci-senha/esqueci-senha.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-esqueci-senha',
   templateUrl: './esqueci-senha.component.html',
-  styleUrls: ['./esqueci-senha.component.css']
+  styleUrls: ['./esqueci-senha.component.css'],
 })
 export class EsqueciSenhaComponent implements OnInit {
+  email: string = '';
+  mensagemSucesso!: string;
+  errors!: String[];
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  goBack() {
+    this.location.back();
   }
-
 }
