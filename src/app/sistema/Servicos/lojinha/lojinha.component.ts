df38ba8a02ead372071cfb27c47089115d8fd5d7
vctrwesley <vctrwesley@gmail.com>
2025-06-06 20:35:03 -0300

    Adicionando componente de produtos à Lojinha com exibição em cards, lógica de paginação e botão para resgatar produtos. Criando a classe Produto para modelagem dos dados.

--- src/app/sistema/Servicos/lojinha/lojinha.component.ts
+++ src/app/sistema/Servicos/lojinha/lojinha.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Permissao } from 'src/app/login/permissao';
 import { AuthService } from 'src/app/services/configs/auth.service';
+import { Produto } from './produto';
 
 @Component({
   selector: 'app-lojinha',
@@ -9,12 +10,62 @@ import { AuthService } from 'src/app/services/configs/auth.service';
   styleUrls: ['./lojinha.component.css'],
 })
 export class LojinhaComponent implements OnInit {
+  mensagemBusca: string = '';
+  isLoading = false;
+  successMessage: string = '';
+  messageTimeout: any;
+
+  produtos: Produto[] = [
+    Object.assign(new Produto(), {
+      id: '1',
+      dataCadastro: '2024-06-01',
+      foto: {
+        documentoUrl: 'assets/imgs/bg-login.png',
+        id: 1,
+        name: 'Caneca',
+      },
+      nome: 'Caneca Personalizada',
+      qtdMoedas: 30,
+      qtdEstoque: 10,
+    }),
+    Object.assign(new Produto(), {
+      id: '1',
+      dataCadastro: '2024-06-01',
+      foto: {
+        documentoUrl: 'assets/imgs/bg-login.png',
+        id: 1,
+        name: 'Caneca',
+      },
+      nome: 'Caneca Personalizada',
+      qtdMoedas: 300,
+      qtdEstoque: 10,
+    }),
+    Object.assign(new Produto(), {
+      id: '1',
+      dataCadastro: '2024-06-01',
+      foto: {
+        documentoUrl: 'assets/imgs/bg-login.png',
+        id: 1,
+        name: 'Caneca',
+      },
+      nome: 'Caneca Personalizada',
+      qtdMoedas: 100,
+      qtdEstoque: 10,
+    }),
+  ];
+
+  itensPorPagina = 6;
+  paginaAtual = 1;
+  totalPaginas = Math.ceil(this.produtos.length / this.itensPorPagina);
+  produtosPaginados: Produto[] = [];
+
   public Permissao = Permissao;
   public cargoUsuario!: Permissao;
 
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.atualizarPaginacao();
     this.authService.obterPerfilUsuario().subscribe((usuario) => {
       this.cargoUsuario = ('ROLE_' + usuario.cargo) as Permissao;
     });
@@ -22,6 +73,21 @@ export class LojinhaComponent implements OnInit {
 
   cadastrarProduto(): void {
     this.router.navigate(['/usuario/cadastro-lojinha-produtos']);
+  }
+
+  atualizarPaginacao(): void {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    this.produtosPaginados = this.produtos.slice(inicio, fim);
+  }
+
+  get totalItens() {
+    return this.produtos.length;
+  }
+
+  onPaginaMudou(novaPagina: number) {
+    this.paginaAtual = novaPagina;
+    this.atualizarPaginacao();
   }
 
   get rotaDashboard(): string {
